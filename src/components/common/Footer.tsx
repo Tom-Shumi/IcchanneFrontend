fa67471b36900c590efeadd53e36fc7ad2c33114
Tom-Shumi <tshumiya281@gmail.com>
2022-01-26 21:49:21 +0900

    add footer

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from 'styles/Footer.module.css'
+
+const Footer: NextPage = () => {
+    return (
+        <div className={styles.footer}>
+            copyright©　癒っちゃんねる管理人
+        </div>
+    )
+}
+
+export default Footer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Header from 'components/common/Header'
+import Footer from 'components/common/Footer'
 import MetaHead from 'components/common/MetaHead'
 import NewThread from 'components/home/NewThread'
 
@@ -9,6 +10,7 @@ const Home: NextPage = () => {
             <MetaHead title='癒っちゃんねる' />
             <Header />
             <NewThread />
+            <Footer />
         </>
     )
 }
