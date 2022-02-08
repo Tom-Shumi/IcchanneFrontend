91bf36fd1372a0e2e9e129bc89eedb65e29fc7d6
Tom-Shumi <tshumiya281@gmail.com>
2022-02-08 21:47:24 +0900

    add google アナリティクス

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import '../styles/globals.css'
+import Head from "next/head";
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { apolloClient }from 'utils/ApolloClientUtils';
+import GoogleAnalytics from 'lib/GoogleAnalytics'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ApolloProvider client={apolloClient}>
-    <Component {...pageProps} />
-  </ApolloProvider>
+  <>
+    <GoogleAnalytics />
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  </>
 )
 
 export default MyApp
