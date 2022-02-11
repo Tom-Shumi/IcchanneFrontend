fa76842aea07028c81f1854da4a206095389cd0e
Tom-Shumi <tshumiya281@gmail.com>
2022-02-11 21:21:33 +0900

    add br

--- src/components/common/AdMobileTop.tsx
+++ src/components/common/AdMobileTop.tsx
@@ -14,13 +14,13 @@ const AdTop: NextPage = () => {
     }, []);
     
     return (
-        <div className={styles.adTop}>
+        <div>
             <a href="https://px.a8.net/svt/ejp?a8mat=3N3SA7+5CB16A+2GMK+ZT0BL" rel="nofollow">
                 <Image width="468" height="60" alt="dmm" src="https://www22.a8.net/svt/bgt?aid=220206319323&wid=001&eno=01&mid=s00000011486006014000&mc=1" />
             </a>
             <Image width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=3N3SA7+5CB16A+2GMK+ZT0BL" alt="" />
-
             <div id="admaxTop"></div>
+            <br />
         </div>
     )
 
