aabf0ed24a7b96144187c03dd86e3328468916ae
Tom-Shumi <tshumiya281@gmail.com>
2022-02-11 14:01:28 +0900

    add pc admax

--- src/components/common/AdRight.tsx
+++ src/components/common/AdRight.tsx
@@ -9,6 +9,8 @@ const AdRight: NextPage = () => {
             <a href="https://rpx.a8.net/svt/ejp?a8mat=3N3RI4+7HPDTE+2HOM+61JSH&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0ea62065.34400275.0ea62066.204f04c0%2Fa22020526994_3N3RI4_7HPDTE_2HOM_61JSH%3Fpc%3Dhttp%253A%252F%252Fwww.rakuten.co.jp%252F%26m%3Dhttp%253A%252F%252Fm.rakuten.co.jp%252F" rel="nofollow">
             <Image width="100" height="500" alt="rakuten" src="http://hbb.afl.rakuten.co.jp/hsb/0eb4bbd6.e4f5b3a4.0eb4bbaa.95151395/" /></a>
             <Image width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=3N3RI4+7HPDTE+2HOM+61JSH" alt="" />
+            
+            <script async src="https://adm.shinobi.jp/s/6c8d5c9b6e9747a2eb6013882c2a8f32" />
         </div>
     )
 
